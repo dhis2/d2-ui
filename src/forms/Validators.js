bae263c3da05928878c882d9174784d50359eb33
Nicolay Ramm <nico@fulcrum.local>
2015-10-21 17:05:26 +0200

    Add validation for relative URLs and lists of URLs

--- src/forms/Validators.js
+++ src/forms/Validators.js
@@ -4,6 +4,8 @@ import isFinite from 'lodash.isfinite';
 // const urlRegExp = /^(([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?$/i;
 const urlRegExp = /^https?:\/\/[^ ]*/i;
 
+const relativeUrlRegExp = /[^ ]*/i;
+
 // Taken from the HTML5 spec http://www.w3.org/TR/html5/forms.html#e-mail-state-(type=email)
 const emailRegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
@@ -20,7 +22,7 @@ export function isEmptyString(value) {
 }
 
 export function isEmptyStringOrUndefined(value) {
-    return isEmptyString(value) || isUndefined(value);
+    return isUndefined(value) || isEmptyString(value);
 }
 
 export function isRequired(value) {
@@ -31,7 +33,26 @@ export function isUrl(value) {
     if (isEmptyStringOrUndefined(value)) {
         return true;
     }
-    return urlRegExp.test(value) || 'field_should_be_an_url';
+    return urlRegExp.test(value) || 'field_should_be_a_url';
+}
+
+export function isRelativeUrl(value) {
+    if (isEmptyStringOrUndefined(value)) {
+        return true;
+    }
+    return relativeUrlRegExp.test(value.trim()) || 'field_should_be_a_relative_url';
+}
+
+export function isUrlArray(value) {
+    if (isEmptyStringOrUndefined(value)) {
+        return true;
+    }
+    return (value + '')
+        .split('\n')
+        .filter(v => v.trim().length > 0)
+        .reduce((prev, curr) => {
+            return prev === true && isUrl(curr) || isEmptyString(curr.trim());
+        }, true) === true ? true : 'field_should_be_list_of_urls';
 }
 
 export function isEmail(value) {
@@ -51,6 +72,8 @@ export function isNumber(value) {
 export const wordToValidatorMap = new Map([
     ['required', isRequired],
     ['url', isUrl],
+    ['relative_url', isRelativeUrl],
+    ['url_array', isUrlArray],
     ['number', isNumber],
     ['email', isEmail],
 ]);
