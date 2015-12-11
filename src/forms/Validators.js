621675961709ab10077d1207447bcd26e2443557
Mark Polak <mark@thedutchies.com>
2015-12-11 12:18:44 +0100

    Add validator tests and refactoring

--- src/forms/Validators.js
+++ src/forms/Validators.js
@@ -4,6 +4,7 @@ import isFinite from 'lodash.isfinite';
 // const urlRegExp = /^(([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?$/i;
 const urlRegExp = /^https?:\/\/[^ ]*/i;
 
+// Don't allow spaces in url?
 const relativeUrlRegExp = /[^ ]*/i;
 
 // Taken from the HTML5 spec http://www.w3.org/TR/html5/forms.html#e-mail-state-(type=email)
@@ -24,24 +25,29 @@ export function isEmptyString(value) {
 export function isEmptyStringOrUndefined(value) {
     return isUndefined(value) || isEmptyString(value);
 }
+isEmptyStringOrUndefined.message = 'value_should_be_empty_string_or_undefined';
 
 export function isRequired(value) {
-    return Boolean(value) || 'field_required';
+    return (Boolean(value) || value === 0);
 }
+isRequired.message = 'value_required';
 
 export function isUrl(value) {
     if (isEmptyStringOrUndefined(value)) {
         return true;
     }
-    return urlRegExp.test(value) || 'field_should_be_a_url';
+    return urlRegExp.test(value);
 }
+isUrl.message = 'value_should_be_a_url';
 
+// FIXME: Always returns true?
 export function isRelativeUrl(value) {
     if (isEmptyStringOrUndefined(value)) {
         return true;
     }
-    return relativeUrlRegExp.test(value.trim()) || 'field_should_be_a_relative_url';
+    return relativeUrlRegExp.test(value.trim());
 }
+isRelativeUrl.message = 'value_should_be_a_relative_url';
 
 export function isUrlArray(value) {
     if (isEmptyStringOrUndefined(value)) {
@@ -52,22 +58,30 @@ export function isUrlArray(value) {
         .filter(v => v.trim().length > 0)
         .reduce((prev, curr) => {
             return prev === true && isUrl(curr) || isEmptyString(curr.trim());
-        }, true) === true ? true : 'field_should_be_list_of_urls';
+        }, true);
 }
+isUrlArray.message = 'value_should_be_list_of_urls';
 
 export function isEmail(value) {
     if (isEmptyStringOrUndefined(value)) {
         return true;
     }
-    return emailRegExp.test(value) || 'field_should_be_an_email';
+    return emailRegExp.test(value);
 }
+isEmail.message = 'value_should_be_an_email';
 
 export function isNumber(value) {
-    if (!value && value !== 0 ) {
+    if (isUndefined(value)) {
         return true;
     }
-    return isFinite(Number(value)) || 'field_should_be_a_number';
+
+    if (value instanceof Number) {
+        return true;
+    }
+
+    return isFinite(value);
 }
+isNumber.message = 'value_should_be_a_number';
 
 export const wordToValidatorMap = new Map([
     ['required', isRequired],
--- test/forms/Validators.test.js
+++ test/forms/Validators.test.js
@@ -1,6 +1,13 @@
 import {
     isEmptyString,
+    isEmptyStringOrUndefined,
     isEmail,
+    isNull,
+    isRequired,
+    isUrl,
+    isUrlArray,
+    isRelativeUrl,
+    isNumber,
 } from '../../src/forms/Validators';
 
 describe('Validators', () => {
@@ -27,13 +34,35 @@ describe('Validators', () => {
         });
     });
 
+    describe('isEmptyStringOrUndefined', () => {
+        it('should return true when the value is an empty string', () => {
+            expect(isEmptyStringOrUndefined('')).to.be.true;
+        });
+
+        it('should return true when the value is undefined', () => {
+            expect(isEmptyStringOrUndefined(undefined)).to.be.true;
+        });
+
+        it('should return false when the value is 0', () => {
+            expect(isEmptyStringOrUndefined(0)).to.be.false;
+        });
+
+        it('should return false when the value is an object', () => {
+            expect(isEmptyStringOrUndefined({})).to.be.false;
+        });
+
+        it('should have a message', () => {
+            expect(isEmptyStringOrUndefined.message).to.equal('value_should_be_empty_string_or_undefined');
+        });
+    });
+
     describe('isEmail', () => {
         it('should return true for a valid email', () => {
             expect(isEmail('superman@gmail.com')).to.be.true;
         });
 
         it('should return false for an invalid email', () => {
-            expect(isEmail('@gmail.com')).to.equal('field_should_be_an_email');
+            expect(isEmail('@gmail.com')).to.be.false;
         });
 
         it('should return true when the value is undefined', () => {
@@ -41,7 +70,7 @@ describe('Validators', () => {
         });
 
         it('should return true when the value is null', () => {
-            expect(isEmail(null)).to.equal('field_should_be_an_email');
+            expect(isEmail(null)).to.be.false;
         });
 
         it('should return true when the value is an empty string', () => {
@@ -49,7 +78,167 @@ describe('Validators', () => {
         });
 
         it('should return false when the value is whitespace string', () => {
-            expect(isEmail(' ')).to.equal('field_should_be_an_email');
+            expect(isEmail(' ')).to.be.false;
+        });
+
+        it('should have a message property', () => {
+            expect(isEmail.message).to.equal('value_should_be_an_email');
+        });
+    });
+
+    describe('isNull', () => {
+        it('should return true when value is null', () => {
+            expect(isNull(null)).to.be.true;
+        });
+
+        it('should return false when the value is a string', () => {
+            expect(isNull('string')).to.be.false;
+        });
+    });
+
+    describe('isRequired', () => {
+        it('should return true when the value is available', () => {
+            expect(isRequired('mark')).to.be.true;
+        });
+
+        it('should return false when the value is undefined', () => {
+            expect(isRequired(undefined)).to.be.false;
+        });
+
+        it('should return false when the value is null', () => {
+            expect(isRequired(null)).to.be.false;
+        });
+
+        it('should return true when the value is 0', () => {
+            expect(isRequired(0)).to.be.true;
+        });
+
+        it('should return false when the value is an empty string', () => {
+            expect(isRequired('')).to.be.false;
+        });
+
+        it('should return true for whitespace string', () => {
+            expect(isRequired('  ')).to.be.true;
+        });
+
+        it('should have a message property', () => {
+            expect(isRequired.message).to.equal('value_required');
+        });
+    });
+
+    describe('isUrl', () => {
+        it('should return true when the url is valid', () => {
+            expect(isUrl('http://dhis2.org')).to.be.true;
+        });
+
+        it('should return true when the url is https', () => {
+            expect(isUrl('http://dhis2.org')).to.be.true;
+        });
+
+        it('should return true when the url is localhost:8080', () => {
+            expect(isUrl('http://localhost:8080')).to.be.true;
+        });
+
+        it('should pass if the value is empty', () => {
+            expect(isUrl('')).to.be.true;
+        });
+
+        it('should pass if the value is undefined', () => {
+            expect(isUrl()).to.be.true;
+        });
+
+        it('should have a message', () => {
+            expect(isUrl.message).to.equal('value_should_be_a_url');
+        });
+    });
+
+    describe('isRelativeUrl', () => {
+        it('should return true when there is no value', () => {
+            expect(isRelativeUrl()).to.be.true;
+        });
+
+        it('should return true when the value is an empty string', () => {
+            expect(isRelativeUrl('')).to.be.true;
+        });
+
+        it('should return true if there is a space in the name', () => {
+            expect(isRelativeUrl('   a ')).to.be.true;
+        });
+
+        it('should have a message', () => {
+            expect(isRelativeUrl.message).to.equal('value_should_be_a_relative_url');
+        });
+    });
+
+    describe('isNumber', () => {
+        it('should return true when the value is 100', () => {
+            expect(isNumber(100)).to.be.true;
+        });
+
+        it('should return true when the value is 1.9', () => {
+            expect(isNumber(1.9)).to.be.true;
+        });
+
+        it('should return true when the value is 0', () => {
+            expect(isNumber(0)).to.be.true;
+        });
+
+        it('should return false when the value is Infinity', () => {
+            expect(isNumber(Infinity)).to.be.false;
+        });
+
+        it('should return false if the value is an object', () => {
+            expect(isNumber({})).to.be.false;
+        });
+
+        it('should return true when the value is a number type', () => {
+            expect(isNumber(Number(2.1))).to.be.true;
+        });
+
+        it('should return false when the object.toString returns a number', () => {
+            expect(isNumber({toString() { return ''; }})).to.be.false;
+        });
+
+        it('should return true if the value is undefined', () => {
+            expect(isNumber()).to.be.true;
+        });
+
+        it('should return false if the number is null', () => {
+            expect(isNumber(null)).to.be.false;
+        });
+
+        it('should have a message', () => {
+            expect(isNumber.message).to.equal('value_should_be_a_number');
+        });
+    });
+
+    describe('isUrlArray', () => {
+        it('should return true when the array is empty', () => {
+            expect(isUrlArray([])).to.be.true;
+        });
+
+        it('should return true when the array has a url', () => {
+            expect(isUrlArray(['http://dhis2.org'])).to.be.true;
+        });
+
+        it('should return false when the array has something else then an url', () => {
+            expect(isUrlArray('http://dhis2.org/ \na1')).to.be.false;
+        });
+
+        it('should return true where there is no value', () => {
+            expect(isUrlArray()).to.be.true;
+        });
+
+        it('should return true when the value is an empty string', () => {
+            expect(isUrlArray()).to.be.true;
+        });
+
+        it('should return false when the value is an object', () => {
+            expect(isUrlArray({})).to.be.false;
+        });
+
+        it('should have a message', () => {
+            expect(isUrlArray.message).to.equal('value_should_be_list_of_urls');
         });
     });
 });
