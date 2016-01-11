d33156e6cd56d92b351bec6a657c926ac50f7c0e
Mark Polak <mark@thedutchies.com>
2016-01-11 17:08:46 +0100

    Thinner rows and add type guess for columns

--- src/data-table/DataTableRow.component.js
+++ src/data-table/DataTableRow.component.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import classes from 'classnames';
 import {isObject} from 'd2-utils';
+import moment from 'moment';
+
+function valueTypeGuess(valueType, value) {
+    switch (valueType) {
+    case 'DATE':
+        return moment(new Date(value)).fromNow();
+    default:
+        break;
+    }
+
+    return value;
+}
+
+function getValueAfterValueTypeGuess(dataSource, columnName) {
+    if (dataSource && dataSource.modelDefinition && dataSource.modelDefinition.modelValidations && dataSource.modelDefinition.modelValidations[columnName]) {
+        return valueTypeGuess(dataSource.modelDefinition.modelValidations[columnName].type, dataSource[columnName]);
+    }
+
+    return dataSource[columnName];
+}
 
 const DataTableRow = React.createClass({
     propTypes: {
@@ -21,7 +41,7 @@ const DataTableRow = React.createClass({
             });
 
         const columns = this.props.columns.map((columnName, index) => {
-            const rowValue = this.props.dataSource[columnName];
+            const rowValue = getValueAfterValueTypeGuess(this.props.dataSource, columnName);
             let displayValue;
 
             if (isObject(rowValue)) {
