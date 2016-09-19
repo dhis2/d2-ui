663450f775623487d14ea2e6c9d775b95f9b28b7
Mark Polak <mark@thedutchies.com>
2016-09-19 11:27:20 +0200

    Add ErrorMessage component for custom form fields

---
+++ src/messages/ErrorMessage.component.js
@@ -0,0 +1,7 @@
+import React, { PropTypes } from 'react';
+import Message from './Message.component';
+import mapProps from '../component-helpers/mapProps';
+
+export default mapProps((props) => ({ style: { color: 'red'}, ...props }), Message);
+
+
---
+++ src/messages/Message.component.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+
+const defaultStyle = {
+    padding: '0.5rem 0',
+};
+
+export default function Message({ style: propsStyle, message }) {
+    const style = Object.assign({}, defaultStyle, propsStyle);
+
+    return (
+        <div style={style}>{ message }</div>
+    );
+}
+Message.propTypes = {
+    style: PropTypes.object,
+    message: PropTypes.string,
+};
