993890e1e4db562f507e7cbd11a4f0f1ea69c11f
Nicolay Ramm <nicolay@dhis2.org>
2017-10-19 15:45:10 +0200

    Add semi-useful search index update script

---
+++ copy-header-sources.sh
@@ -0,0 +1,4 @@
+
+cp -v ../maintenance-app/src/config/maintenance-models.js src/app-header/search/sources/maintenance-app/maintenance-models.js
+cp -v ../settings-app/src/settingsCategories.js src/app-header/search/sources/settings-app/settingsCategories.js
+
