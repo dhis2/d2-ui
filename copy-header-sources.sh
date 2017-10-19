8b21ee8c61b47a0f4479ff35e7ee075db3f90c22
Nicolay Ramm <nicolay@dhis2.org>
2017-10-19 16:47:52 +0200

    Make the index copy script slightly less useless

--- copy-header-sources.sh
+++ copy-header-sources.sh
@@ -1,4 +1,21 @@
 
-cp -v ../maintenance-app/src/config/maintenance-models.js src/app-header/search/sources/maintenance-app/maintenance-models.js
-cp -v ../settings-app/src/settingsCategories.js src/app-header/search/sources/settings-app/settingsCategories.js
+read -p "Do you have an up-to-date version of the maintenance app located at ../maintenance-app ? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo -n "Copying maintenance app search index... "
+  cp ../maintenance-app/src/config/maintenance-models.js src/app-header/search/sources/maintenance-app/maintenance-models.js && echo "done!"
+else
+  echo "Skipping..."
+fi
 
+
+read -p "Do you have an up-to-date version of the settings app located at ../settings-app ? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo -n "Copying settings app search index... "
+  cp ../settings-app/src/settingsCategories.js src/app-header/search/sources/settings-app/settingsCategories.js && echo "done!"
+else
+  echo "Skipping..."
+fi
