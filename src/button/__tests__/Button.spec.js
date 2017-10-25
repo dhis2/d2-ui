c12eec8c7475cbee71ccd8dfb3b7aa099663d813
Bjørn Sandvik <bjorn@mastermaps.com>
2017-10-25 11:28:28 +0200

    Removed unused function

--- src/button/__tests__/Button.spec.js
+++ src/button/__tests__/Button.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
