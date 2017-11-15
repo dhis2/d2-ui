66face89fc5a8eb1a4a6308ffe94921cca034342
Jen Jones Arnesen <jarnesen@baosystems.com>
2017-11-15 14:45:52 +0100

    Remove dynamic imports and just import all icons into this wrapper

--- examples/svg-icon/index.js
+++ examples/svg-icon/index.js
@@ -22,10 +22,12 @@ const children = <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />;
 const icons = (
     <MuiThemeProvider muiTheme={getMuiTheme()}>
         <div style={style}>
-            <SvgIcon icon={'star'} />
-            <SvgIcon icon={'star'} style={styles} />
-            <SvgIcon icon={'star'} color="blue" hoverColor="yellow" />
+            <SvgIcon icon={'nav/cancel'} />
+            <SvgIcon icon={'toggle/star'} style={styles} />
+            <SvgIcon icon={'action/info-outline'} color="blue" hoverColor="yellow" />
             <SvgIcon />
+            <SvgIcon icon={'action/visibility'} />
+            <SvgIcon icon={'nothtere'} />
             <SvgIcon>
                 {children}
             </SvgIcon>
--- src/svg-icon/SvgIcon.js
+++ src/svg-icon/SvgIcon.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ToggleStar from 'material-ui/svg-icons/toggle/star';
+import NavArrowDropRight from 'material-ui/svg-icons/navigation-arrow-drop-right';
+import NavClose from 'material-ui/svg-icons/navigation/close';
+import NavArrowUpward from 'material-ui/svg-icons/navigation/arrow-upward';
+import NavArrowDownward from 'material-ui/svg-icons/navigation/arrow-downward';
+import NavChevronLeft from 'material-ui/svg-icons/navigation/chevron-left';
+import NavChevronRight from 'material-ui/svg-icons/navigation/chevron-right';
+import NavMoreVert from 'material-ui/svg-icons/navigation/more-vert';
+import NavCancel from 'material-ui/svg-icons/navigation/cancel';
+import ActionInfoOutline from 'material-ui/svg-icons/action/info-outline';
+import ActionRoom from 'material-ui/svg-icons/action/room';
+import ActionViewList from 'material-ui/svg-icons/action/view-list';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ActionVisibility from 'material-ui/svg-icons/action/visibility';
+import ActionVisibilityOff from 'material-ui/svg-icons/action/visibility-off';
+import ContentCreate from 'material-ui/svg-icons/content/create';
+import EdDragHandle from 'material-ui/svg-icons/editor/drag-handle';
+import SentimentDissatisfied from 'material-ui/svg-icons/social/sentiment-dissatisfied';
+import MUISvgIcon from 'material-ui/SvgIcon';
 import { getRestProps } from '../component-helpers/utils';
 
+
 const icons = {
-    star: 'star',
-    visibility: 'visibility',
-};
-
-const fetchComponent = (icon) => {
-    switch (icon) {
-    case icons.star:
-        return import('material-ui/svg-icons/toggle/star');
-    case icons.visibility:
-        return import('material-ui/svg-icons/action/visibility');
-
-    default:
-        return import('material-ui/SvgIcon');
-    }
+    'toggle/star': ToggleStar,
+    'nav/arrow-drop-right': NavArrowDropRight,
+    'nav/close': NavClose,
+    'nav/arrow-upward': NavArrowUpward,
+    'nav/arrow-downward': NavArrowDownward,
+    'nav/chevron-left': NavChevronLeft,
+    'nav/chevron-right': NavChevronRight,
+    'nav/more-vert': NavMoreVert,
+    'nav/cancel': NavCancel,
+    'action/info-outline': ActionInfoOutline,
+    'action/room': ActionRoom,
+    'action/view-list': ActionViewList,
+    'action/delete': ActionDelete,
+    'action/visibility': ActionVisibility,
+    'action/visibility-off': ActionVisibilityOff,
+    'content/create': ContentCreate,
+    'edit/drag-handle': EdDragHandle,
 };
 
 export default class SvgIcon extends React.Component {
@@ -25,24 +48,19 @@ export default class SvgIcon extends React.Component {
     componentWillMount() {
         const { icon, children } = this.props;
 
-        if (!icons.hasOwnProperty(icon) && !children) {
-            return;
+        if (children && !icons[icon]) {
+            this.setState({ Icon: MUISvgIcon });
+        } else {
+            const component = icons[icon] || SentimentDissatisfied;
+            this.setState({ Icon: component });
         }
-
-        fetchComponent(icon)
-            .then(component => this.setState({ Icon: component.default }));
     }
 
     render() {
         const { Icon } = this.state;
         const omitProps = [
             'icon',  // icon used internally only
-            'color', // color is a v0.19 prop and not supported in v1.0.0 (use style property instead)
         ];
-
-        if (!Icon) {
-            return <div>Loading icon...</div>;
-        }
 
         const rest = getRestProps(this.props, omitProps);
         const { children } = this.props;
