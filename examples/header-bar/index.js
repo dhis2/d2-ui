4a20cd0958f64e6f4401119296707414ce0aa401
Nicolay Ramm <nicolay@dhis2.org>
2017-11-29 17:52:22 +0100

    Remove deprecated index.js files

--- examples/header-bar/index.js
+++ examples/header-bar/index.js
@@ -1,5 +1,5 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import { initHeaderBar } from '../../src/app-header';
+import { initHeaderBar } from '../../src/app-header/initHeaderBar';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
