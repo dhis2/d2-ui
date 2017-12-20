cdf0b8f019ce80cf374b29be3326cd7c94919a5f
Nicolay Ramm <nicolay@dhis2.org>
2017-12-20 17:52:48 +0100

    Tweak the Chip example

--- examples/chip/index.js
+++ examples/chip/index.js
@@ -15,11 +15,11 @@ const chipFactory = (label, index) => (
     <Chip
         key={`${label}.${index}`}
         label={label}
-        color={index % 2 ? 'primary' : 'default'}
+        color={index % 2 === 0 ? 'primary' : 'default'}
         avatar={index % 3 === 0 ? 'star' : null}
-        disabled={index % 4 === 1}
+        disabled={index % 4 === 0}
         onClick={(...args) => console.log.apply(null, ['Click:', ...args])}
-        onRequestDelete={index % 4 === 2 ? (...args) => console.log.apply(null, ['Delete:', ...args]) : null}
+        onRequestDelete={index % 5 === 0 ? (...args) => console.log.apply(null, ['Delete:', ...args]) : null}
     />
 );
 
@@ -34,24 +34,39 @@ const floatStyle = {
 const floatLeftStyle = Object.assign({}, floatStyle, { float: 'left' });
 const floatRightStyle = Object.assign({}, floatStyle, { float: 'right', padding: '7px 32px' });
 
+const chipLabels = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'Long dashboard name that is long and has lots of text',
+    'Here a chip',
+    'There a chip',
+    'Everywhere a chip chip',
+    'Hello',
+    'Another dashboard',
+    'Click me for fun!',
+    'Don\'t click!',
+    'FCK',
+    'More chips',
+    'Banana chips',
+    'Potato chips',
+    'Computer chips'
+];
 const buttons = (
     <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <div>
-            <div style={style}>
-                <div style={floatLeftStyle}>Float left</div>
-                <div style={floatRightStyle}>Float right</div>
-                <Chip label={'Default'}/>
-                <Chip label={'With avatar'} avatar={'star'}/>
-                <Chip label={'Primary'} color={'primary'}/>
-                <Chip label={'With avatar'} avatar={'star'} color={'primary'}/>
-                <Chip label={'With onClick'} onClick={() => alert('clicked')}/>
-                <Chip label={'With onRequestDelete'} onRequestDelete={() => alert('delete')}/>
-                <Chip label={'Disabled'} disabled/>
-                <Chip label={'With avatar'} avatar={'star'} disabled/>
-                <Chip label={'With onClick'} onClick={() => alert('clicked')} disabled/>
-                <Chip label={'With onRequestDelete'} onRequestDelete={() => alert('delete')} disabled/>
-                {'Long dashboard name that is long as has lots of text|Here a chip|There a chip|Everywhere a chip chip|Hello|Another dashboard|Click me for fun!|Don\'t click!|FCK|More chips|Banana chips|Potato chips|Computer chips'.split('|').map((x, i) => chipFactory(x, i))}
-            </div>
+        <div style={style}>
+            <div style={floatLeftStyle}>Float left</div>
+            <div style={floatRightStyle}>Float right</div>
+            <Chip label={'Default'}/>
+            <Chip label={'With avatar'} avatar={'star'}/>
+            <Chip label={'Primary'} color={'primary'}/>
+            <Chip label={'With avatar'} avatar={'star'} color={'primary'}/>
+            <Chip label={'With onClick'} onClick={() => alert('clicked')}/>
+            <Chip label={'With onRequestDelete'} onRequestDelete={() => alert('delete')}/>
+            <Chip label={'Disabled'} disabled/>
+            <Chip label={'With avatar'} avatar={'star'} disabled/>
+            <Chip label={'With onClick'} onClick={() => alert('clicked')} disabled/>
+            <Chip label={'With onRequestDelete'} onRequestDelete={() => alert('delete')} disabled/>
+            {chipLabels.map(chipFactory)}
         </div>
     </MuiThemeProvider>
 );
