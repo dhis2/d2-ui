ad07518a774997d586648c84a9c2a39ef12e09fe
Jen Jones Arnesen <jarnesen@baosystems.com>
2017-12-20 13:18:49 +0100

    Prefer arrow function and add tests

--- src/form-fields/__tests__/TextField.component.spec.js
+++ src/form-fields/__tests__/TextField.component.spec.js
@@ -1,5 +1,31 @@
-import '../TextField';
+import React from 'react';
+import { shallow } from 'enzyme';
 
-xdescribe('TextField component', () => {
-    it('has no tests');
+import MuiTextField from 'material-ui/TextField';
+import TextField from '../TextField';
+
+describe('TextField component', () => {
+    let Component;
+    let onChangeSpy;
+    const originalTextVal = 'The value of the text field';
+
+    beforeEach(() => {
+        onChangeSpy = jest.fn();
+
+        Component = shallow(
+            <TextField value={originalTextVal} changeEvent={onChangeSpy} />);
+    });
+
+    it('should render a TextField component', () => {
+        expect(Component.type()).toBe(MuiTextField);
+    });
+
+    it('should change the local state when field content is changed', () => {
+        expect(Component.state().value).toEqual(originalTextVal);
+
+        const newTextVal = 'New text field value';
+        Component.simulate('change', {}, newTextVal);
+
+        expect(Component.state().value).toEqual(newTextVal);
+    });
 });
