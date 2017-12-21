fa9a925e0a3855e84bdd92c36f740614036813d6
Edoardo Sabadelli <edoardo@dhis2.org>
2017-12-21 16:15:28 +0100

    Replace MUI FontIcon with d2-ui SvgIcon

--- src/sharing/Access.component.js
+++ src/sharing/Access.component.js
@@ -1,61 +1,63 @@
-import PropTypes from 'prop-types';
-import React from 'react';
-import { compose, mapProps, getContext, withProps } from 'recompose';
-import FontIcon from 'material-ui/FontIcon';
-import { config } from 'd2/lib/d2';
-import IconButton from 'material-ui/IconButton';
+import PropTypes from "prop-types";
+import React from "react";
+import { compose, mapProps, getContext, withProps } from "recompose";
+import { config } from "d2/lib/d2";
+import IconButton from "material-ui/IconButton";
+import SvgIcon from "../svg-icon/SvgIcon";
 
-import PermissionPicker from './PermissionPicker.component';
+import PermissionPicker from "./PermissionPicker.component";
 
-import {
-    accessStringToObject,
-    accessObjectToString,
-} from './utils';
+import { accessStringToObject, accessObjectToString } from "./utils";
 
-config.i18n.strings.add('public_access');
-config.i18n.strings.add('external_access');
-config.i18n.strings.add('anyone_can_view_without_a_login');
-config.i18n.strings.add('anyone_can_find_view_and_edit');
-config.i18n.strings.add('anyone_can_find_and_view');
-config.i18n.strings.add('no_access');
+config.i18n.strings.add("public_access");
+config.i18n.strings.add("external_access");
+config.i18n.strings.add("anyone_can_view_without_a_login");
+config.i18n.strings.add("anyone_can_find_view_and_edit");
+config.i18n.strings.add("anyone_can_find_and_view");
+config.i18n.strings.add("no_access");
 
 const styles = {
     accessView: {
-        fontWeight: '400',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '4px 8px',
+        fontWeight: "400",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "4px 8px"
     },
     accessDescription: {
-        display: 'flex',
-        flexDirection: 'column',
+        display: "flex",
+        flexDirection: "column",
         flex: 1,
-        paddingLeft: 16,
-    },
+        paddingLeft: 16
+    }
 };
 
 const d2Context = {
-    d2: PropTypes.object.isRequired,
+    d2: PropTypes.object.isRequired
 };
 
-const getAccessIcon = (userType) => {
+const getAccessIcon = userType => {
     switch (userType) {
-    case 'user': return 'person';
-    case 'userGroup': return 'group';
-    case 'external': return 'public';
-    case 'public': return 'business';
-    default: return 'person';
+        case "user":
+            return "Person";
+        case "userGroup":
+            return "Group";
+        case "external":
+            return "Public";
+        case "public":
+            return "Business";
+        default:
+            return "Person";
     }
 };
 
 const useAccessObjectFormat = props => ({
     ...props,
     access: accessStringToObject(props.access),
-    onChange: (newAccess) => {
+    onChange: newAccess => {
         props.onChange(accessObjectToString(newAccess));
-    },
+    }
 });
 
 export const Access = ({
@@ -66,30 +68,29 @@ export const Access = ({
     secondaryText,
     onChange,
     onRemove,
-    disabled,
+    disabled
 }) => (
     <div style={styles.accessView}>
-        <FontIcon className="material-icons">
-            {getAccessIcon(accessType)}
-        </FontIcon>
+        <SvgIcon icon={getAccessIcon(accessType)} />
         <div style={styles.accessDescription}>
             <div>{primaryText}</div>
-            <div style={{ color: '#818181', paddingTop: 4 }}>{secondaryText || ' '}</div>
+            <div style={{ color: "#818181", paddingTop: 4 }}>
+                {secondaryText || " "}
+            </div>
         </div>
-
         <PermissionPicker
             access={access}
             accessOptions={accessOptions}
             onChange={onChange}
             disabled={disabled}
         />
-
         <IconButton
             disabled={!onRemove}
-            iconStyle={{ color: '#bbbbbb' }}
-            iconClassName="material-icons"
+            iconStyle={{ color: "#bbbbbb" }}
             onClick={onRemove || (() => {})}
-        >clear</IconButton>
+        >
+            <SvgIcon icon="Clear" />
+        </IconButton>
     </div>
 );
 
@@ -103,13 +104,13 @@ Access.propTypes = {
     onChange: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
     secondaryText: PropTypes.string,
-    onRemove: PropTypes.func,
+    onRemove: PropTypes.func
 };
 
 Access.defaultProps = {
     secondaryText: undefined,
     onRemove: undefined,
-    disabled: false,
+    disabled: false
 };
 
 export const GroupAccess = compose(
@@ -119,52 +120,60 @@ export const GroupAccess = compose(
         primaryText: props.groupName,
         accessOptions: {
             meta: { canView: true, canEdit: true, noAccess: false },
-            data: props.dataShareable && { canView: true, canEdit: true, noAccess: true },
-        },
-    })),
+            data: props.dataShareable && {
+                canView: true,
+                canEdit: true,
+                noAccess: true
+            }
+        }
+    }))
 )(Access);
 
 export const ExternalAccess = compose(
     getContext(d2Context),
     withProps(props => ({
-        accessType: 'external',
-        primaryText: props.d2.i18n.getTranslation('external_access'),
+        accessType: "external",
+        primaryText: props.d2.i18n.getTranslation("external_access"),
         secondaryText: props.access
-            ? props.d2.i18n.getTranslation('anyone_can_view_without_a_login')
-            : props.d2.i18n.getTranslation('no_access'),
+            ? props.d2.i18n.getTranslation("anyone_can_view_without_a_login")
+            : props.d2.i18n.getTranslation("no_access"),
         access: {
             meta: { canEdit: false, canView: props.access },
-            data: { canEdit: false, canView: false },
+            data: { canEdit: false, canView: false }
         },
-        onChange: (newAccess) => {
+        onChange: newAccess => {
             props.onChange(newAccess.meta.canView);
         },
         accessOptions: {
-            meta: { canView: true, canEdit: false, noAccess: true },
-        },
-    })),
+            meta: { canView: true, canEdit: false, noAccess: true }
+        }
+    }))
 )(Access);
 
 const constructSecondaryText = ({ canView, canEdit }) => {
     if (canEdit) {
-        return 'anyone_can_find_view_and_edit';
+        return "anyone_can_find_view_and_edit";
     }
 
-    return canView
-        ? 'anyone_can_find_and_view'
-        : 'no_access';
+    return canView ? "anyone_can_find_and_view" : "no_access";
 };
 
 export const PublicAccess = compose(
     mapProps(useAccessObjectFormat),
     getContext(d2Context),
     withProps(props => ({
-        accessType: 'public',
-        primaryText: props.d2.i18n.getTranslation('public_access'),
-        secondaryText: props.d2.i18n.getTranslation(constructSecondaryText(props.access.meta)),
+        accessType: "public",
+        primaryText: props.d2.i18n.getTranslation("public_access"),
+        secondaryText: props.d2.i18n.getTranslation(
+            constructSecondaryText(props.access.meta)
+        ),
         accessOptions: {
             meta: { canView: true, canEdit: true, noAccess: true },
-            data: props.dataShareable && { canView: true, canEdit: true, noAccess: true },
-        },
-    })),
+            data: props.dataShareable && {
+                canView: true,
+                canEdit: true,
+                noAccess: true
+            }
+        }
+    }))
 )(Access);
--- src/sharing/PermissionOption.component.js
+++ src/sharing/PermissionOption.component.js
@@ -1,7 +1,7 @@
-import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import FontIcon from 'material-ui/FontIcon';
-import MenuItem from 'material-ui/MenuItem';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import SvgIcon from "../svg-icon/SvgIcon";
+import MenuItem from "material-ui/MenuItem";
 
 class PermissionOption extends Component {
     ref = null;
@@ -15,9 +15,7 @@ class PermissionOption extends Component {
             <MenuItem
                 insetChildren
                 leftIcon={
-                    <FontIcon className="material-icons">
-                        {this.props.isSelected ? 'done' : ''}
-                    </FontIcon>
+                    this.props.isSelected ? <SvgIcon icon="Done" /> : undefined
                 }
                 primaryText={this.props.primaryText}
                 value={this.props.value}
@@ -26,7 +24,7 @@ class PermissionOption extends Component {
                 focusState={this.props.focusState}
             />
         );
-    }
+    };
 }
 
 PermissionOption.propTypes = {
@@ -35,13 +33,13 @@ PermissionOption.propTypes = {
     primaryText: PropTypes.string.isRequired,
     value: PropTypes.object.isRequired,
     onClick: PropTypes.func,
-    focusState: PropTypes.string,
+    focusState: PropTypes.string
 };
 
 PermissionOption.defaultProps = {
     onClick: undefined,
-    focusState: 'none',
+    focusState: "none"
 };
 
-PermissionOption.muiName = 'MenuItem';
+PermissionOption.muiName = "MenuItem";
 export default PermissionOption;
