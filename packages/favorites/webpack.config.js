c5c406b14cc0a2006208b090cc70743f03afd638
Viktor Varland <viktor@dhis2.org>
2018-03-21 15:00:36 +0100

    Refactor build process for favorites

--- packages/favorites/webpack.config.js
+++ packages/favorites/webpack.config.js
@@ -1,25 +1,40 @@
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
+const rxPaths = require('rxjs/_esm5/path-mapping');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
-        library: 'FavoritesDialog',
+        library: 'SharingDialog',
         libraryTarget: 'umd',
     },
     module: {
-        loaders: [
-			{ test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-			{ test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-        ]
+        rules: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+        ],
     },
+    resolve: {
+        alias: rxPaths(),
+    },
+    devtool: 'source-map',
     externals: {
-        "react": {
+        react: {
             root: 'React',
             amd: 'react',
             commonjs2: 'react',
-            commonjs: 'react'
-        }
-    }
-}
+            commonjs: 'react',
+        },
+        'react-dom': {
+            root: 'ReactDOM',
+            commonjs2: 'react-dom',
+            commonjs: 'react-dom',
+            amd: 'react-dom',
+            umd: 'react-dom',
+        },
+    },
+
+    plugins: [new webpack.optimize.ModuleConcatenationPlugin()],
+};
