621573d9ca431614859006af7b04bd78e6bd1a56
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-04-10 13:47:52 +0200

    Re-add the Translate component in CurrentIcon, and add a few tests

--- packages/core/src/icon-picker/__tests__/CurrentIcon.component.spec.js
+++ packages/core/src/icon-picker/__tests__/CurrentIcon.component.spec.js
@@ -1,5 +1,44 @@
-import '../CurrentIcon.component';
+import React from 'react';
+import { shallow } from 'enzyme';
+import FlatButton from 'material-ui/FlatButton/FlatButton';
+import CurrentIcon from '../CurrentIcon.component';
+import IconOption from '../IconOption.component';
 
-xdescribe('CurrentIcon component', () => {
-    it('has no tests');
+
+describe('CurrentIcon component', () => {
+    let props;
+    let shallowComponent;
+    const currentIcon = () => {
+        if (!shallowComponent) {
+            shallowComponent = shallow(<CurrentIcon {...props} />);
+        }
+        return shallowComponent;
+    };
+
+    beforeEach(() => {
+        props = {
+            controlsStyle: undefined,
+            dashboards: undefined,
+        };
+        shallowComponent = undefined;
+    });
+
+    describe('when no Img source is provided', () => {
+        it('renders a FlatButton', () => {
+            expect(currentIcon().find(FlatButton)).toHaveLength(1);
+        });
+
+        it('triggers onIconClicked when button is clicked', () => {
+            props.onIconClicked = jest.fn();
+            const component = currentIcon();
+            component.simulate('click');
+            expect(props.onIconClicked).toBeCalled();
+        })
+    });
+
+    it('renders an IconOption when Img source is provided', () => {
+        props.imgSrc = 'abc123';
+        expect(currentIcon().find(IconOption)).toHaveLength(1);
+        expect(currentIcon().find(FlatButton)).toHaveLength(0);
+    });
 });
