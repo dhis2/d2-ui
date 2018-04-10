3f41ac701dbf13a26c3818425964e95ad1fcf05d
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-04-10 13:29:32 +0200

    export the Translate component as a named export

--- packages/translation-dialog/src/index.js
+++ packages/translation-dialog/src/index.js
@@ -1,2 +1,5 @@
-import TranslationDialog from './TranslationDialog.component'
-export default TranslationDialog
+import TranslationDialog from './TranslationDialog.component';
+
+export { default as Translate } from './Translate.component';
+
+export default TranslationDialog;
