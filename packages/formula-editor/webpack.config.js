11354f0b944a4283d68c1112b7192ac37b7de6d0
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-04-11 12:48:09 +0200

    Use correct library name in webpack config

--- packages/formula-editor/webpack.config.js
+++ packages/formula-editor/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
-        library: 'SharingDialog',
+        library: 'FormulaEditor',
         libraryTarget: 'umd',
     },
     module: {
