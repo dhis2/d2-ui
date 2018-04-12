8261beeeee7fc551c62a60c12294e5f3ffe2cd76
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-04-12 15:45:00 +0200

    Re-add the Translation component

--- packages/data-table/src/data-value/PublicAccessValue.component.js
+++ packages/data-table/src/data-value/PublicAccessValue.component.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import { isString } from 'lodash/fp';
 import { isFunction } from 'lodash/fp';
+import log from 'loglevel';
 import { addD2Context } from 'd2-ui';
 
-export const TranslatedSpan = ({ props, context }) => {
+function Translate(props, context) {
     if (context.d2 && context.d2.i18n && isFunction(context.d2.i18n.getTranslation)) {
-        return <span>{context.d2.i18n.getTranslation(props.children)}</span>;
+        return (<span>{context.d2.i18n.getTranslation(props.children)}</span>);
     }
 
+    log.error('<Translate />: d2 is not available on the `context`');
     return <span />;
-};
+}
 
-const Translate = addD2Context(TranslatedSpan);
+const TranslateSpan = addD2Context(Translate);
 
 const PublicAccessValue = ({ value }) => {
     const metaData = value.substr(0, 2);
@@ -19,17 +22,15 @@ const PublicAccessValue = ({ value }) => {
 
     if (other === '----' && (data === '--' || data === 'r-' || data === 'rw')) {
         if (metaData === 'rw') {
-            return <span>public_can_edit</span>;
+            return <TranslateSpan>public_can_edit</TranslateSpan>;
         } else if (metaData === 'r-') {
-            return <span>public_can_view</span>;
+            return <TranslateSpan>public_can_view</TranslateSpan>;
         } else if (metaData === '--') {
-            return <span>public_none</span>;
+            return <TranslateSpan>public_none</TranslateSpan>;
         }
 
-        return <span />;
-
+        return null;
     }
-
 };
 
 export default PublicAccessValue;
