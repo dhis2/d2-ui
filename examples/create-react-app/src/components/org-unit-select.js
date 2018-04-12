fd6732c1f38e512ecfe7a9713fed93c3f806040e
Viktor Varland <viktor@dhis2.org>
2018-04-12 16:36:04 +0200

    Fix the org-unit-select example (kinda)

--- examples/create-react-app/src/components/org-unit-select.js
+++ examples/create-react-app/src/components/org-unit-select.js
@@ -6,16 +6,15 @@ import Card from 'material-ui/Card/Card';
 import CardText from 'material-ui/Card/CardText';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import { OrgUnitTree } from 'd2-ui-org-unit-select';
+import { OrgUnitTree } from 'd2-ui-org-unit-tree';
 import { OrgUnitSelectByLevel } from 'd2-ui-org-unit-select';
 import { OrgUnitSelectByGroup } from 'd2-ui-org-unit-select';
 import { OrgUnitSelectAll } from 'd2-ui-org-unit-select';
 
 import { TreeView } from 'd2-ui-core';
 
-import { mergeChildren, incrementMemberCount, decrementMemberCount } from '../../src/org-unit-tree/utils';
+import { mergeChildren, incrementMemberCount, decrementMemberCount } from 'd2-ui-org-unit-tree';
 
 const styles = {
     card: {
@@ -63,9 +62,42 @@ export default class OrgUnitSelectExample extends React.Component {
         super(props);
 
         this.state = {
-            selected: props.selected,
-            rootWithMembers: props.rootWithMembers,
+            d2: props.d2,
         };
+
+        Promise.all([
+            props.d2.models.organisationUnitLevels.list({
+                paging: false,
+                fields: 'id,level,displayName',
+                order: 'level:asc',
+            }),
+            props.d2.models.organisationUnitGroups.list({
+                paging: false,
+                fields: 'id,displayName',
+            }),
+            props.d2.models.organisationUnits.list({
+                paging: false,
+                level: 1,
+                fields: 'id,displayName,path,children::isNotEmpty,memberCount',
+                memberCollection: 'dataSets',
+                memberObject: 'TuL8IOPzpHh',
+            }),
+            props.d2.models.dataSets.get('TuL8IOPzpHh', {
+                paging: false,
+                fields: 'organisationUnits[id,path]',
+            }),
+        ])
+        .then(([levels, groups, rootWithDataSetMembers, dataSetMembers]) => {
+            const rootWithMembers = rootWithDataSetMembers.toArray()[0];
+            const selected = dataSetMembers.organisationUnits.toArray().map(ou => ou.path);
+
+            this.setState({
+                levels,
+                rootWithMembers,
+                selected,
+                groups
+            })
+        });
 
         this.handleSelectionUpdate = this.handleSelectionUpdate.bind(this);
         this.handleOrgUnitClick = this.handleOrgUnitClick.bind(this);
@@ -113,40 +145,11 @@ export default class OrgUnitSelectExample extends React.Component {
             this.setState({ currentRoot });
         };
 
-        await Promise.all([
-            d2.models.organisationUnitLevels.list({
-                paging: false,
-                fields: 'id,level,displayName',
-                order: 'level:asc',
-            }),
-            d2.models.organisationUnitGroups.list({
-                paging: false,
-                fields: 'id,displayName',
-            }),
-            d2.models.organisationUnits.list({
-                paging: false,
-                level: 1,
-                fields: 'id,displayName,path,children::isNotEmpty,memberCount',
-                memberCollection: 'dataSets',
-                memberObject: 'TuL8IOPzpHh',
-            }),
-            d2.models.dataSets.get('TuL8IOPzpHh', {
-                paging: false,
-                fields: 'organisationUnits[id,path]',
-            }),
-        ])
-
-        .then(([levels, groups, rootWithDataSetMembers, dataSetMembers]) => {
-            const rootWithMembers = rootWithDataSetMembers.toArray()[0];
-            const selected = dataSetMembers.organisationUnits.toArray().map(ou => ou.path);
-            render(<OrgUnitSelectExample
-                d2={d2}
-                levels={levels}
-                groups={groups}
-                rootWithMembers={rootWithMembers}
-                selected={selected}
-            />);
-        });
+        const state = this.state;
+        if (!state.levels || !state.selected) {
+            console.info('What is state?', state);
+            return null;
+        }
 
         return (
             <MuiThemeProvider muiTheme={getMuiTheme()}>
@@ -155,7 +158,7 @@ export default class OrgUnitSelectExample extends React.Component {
                         <CardText style={styles.cardText}>
                             <h3 style={styles.cardHeader}>Select By Org Unit Level</h3>
                             <OrgUnitSelectByLevel
-                                levels={this.props.levels}
+                                levels={this.state.levels}
                                 selected={this.state.selected}
                                 onUpdateSelection={this.handleSelectionUpdate}
                             />
@@ -165,7 +168,7 @@ export default class OrgUnitSelectExample extends React.Component {
                         <CardText style={styles.cardText}>
                             <h3 style={styles.cardHeader}>Select by Org Unit Group</h3>
                             <OrgUnitSelectByGroup
-                                groups={this.props.groups}
+                                groups={this.state.groups}
                                 selected={this.state.selected}
                                 onUpdateSelection={this.handleSelectionUpdate}
                             />
@@ -176,10 +179,10 @@ export default class OrgUnitSelectExample extends React.Component {
                             <h3 style={styles.cardHeader}>Selected: {this.state.selected.length}</h3>
                             <div style={styles.left}>
                                 <OrgUnitTree
-                                    root={this.props.rootWithMembers}
+                                    root={this.state.rootWithMembers}
                                     selected={this.state.selected}
                                     currentRoot={this.state.currentRoot}
-                                    initiallyExpanded={[`/${this.props.rootWithMembers.id}`]}
+                                    initiallyExpanded={[`/${this.state.rootWithMembers.id}`]}
                                     onSelectClick={this.handleOrgUnitClick}
                                     onChangeCurrentRoot={changeRoot}
                                     memberCollection="dataSets"
@@ -196,7 +199,7 @@ export default class OrgUnitSelectExample extends React.Component {
                                     ) : <div>For all organisation units:</div>}
                                     <div style={{ marginBottom: -24, marginTop: -16 }}>
                                         <OrgUnitSelectByLevel
-                                            levels={this.props.levels}
+                                            levels={this.state.levels}
                                             selected={this.state.selected}
                                             currentRoot={this.state.currentRoot}
                                             onUpdateSelection={this.handleSelectionUpdate}
@@ -204,7 +207,7 @@ export default class OrgUnitSelectExample extends React.Component {
                                     </div>
                                     <div>
                                         <OrgUnitSelectByGroup
-                                            groups={this.props.groups}
+                                            groups={this.state.groups}
                                             selected={this.state.selected}
                                             currentRoot={this.state.currentRoot}
                                             onUpdateSelection={this.handleSelectionUpdate}
@@ -238,14 +241,6 @@ export default class OrgUnitSelectExample extends React.Component {
 
 OrgUnitSelectExample.propTypes = {
     d2: PropTypes.object.isRequired,
-    levels: PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.array,
-    ]).isRequired,
-    groups: PropTypes.oneOfType([
-        PropTypes.object,
-        PropTypes.array,
-    ]).isRequired,
 };
 
 OrgUnitSelectExample.childContextTypes = { d2: PropTypes.object.isRequired };
