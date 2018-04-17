50b522429b3db124f3c65c07b111a2eb0812a169
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-04-17 15:35:32 +0200

    rename headerbar to header-bar

--- examples/create-react-app/src/components/header-bar.js
+++ examples/create-react-app/src/components/header-bar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeaderBar from 'd2-ui-headerbar';
+import HeaderBar from 'd2-ui-header-bar';
 
 export default function(props) {
     return (<HeaderBar d2={props.d2}/>)
