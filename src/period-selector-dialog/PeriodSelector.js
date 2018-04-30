d7e03ab10459065174a9e2e3a012561f74401b7e
Ilya Nee <neeilya@mail.ru>
2018-04-30 16:30:05 +0200

    Enable redux devtools

--- src/period-selector-dialog/PeriodSelector.js
+++ src/period-selector-dialog/PeriodSelector.js
@@ -4,7 +4,10 @@ import { Provider } from 'react-redux';
 import Periods from './Periods';
 import reducers from './reducers';
 
-const store = createStore(reducers);
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 class PeriodSelector extends Component {
     constructor(props) {
