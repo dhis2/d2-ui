e7b2fddf97b764a86050618527ab484e46369d19
Ilya Nee <neeilya@mail.ru>
2018-05-15 09:20:49 +0200

    Actions and action creators for period selector dialog

--- src/period-selector-dialog/actions/index.js
+++ src/period-selector-dialog/actions/index.js
@@ -1,16 +1,39 @@
 import actionTypes from './actionTypes';
 
 export const setPeriodType = periodType => ({
-        type: actionTypes.SET_PERIOD_TYPE,
-        payload: periodType,
+    type: actionTypes.SET_PERIOD_TYPE,
+    payload: periodType,
 });
 
-export const addSelectedPeriod = period => ({
-    type: actionTypes.ADD_SELECTED_PERIOD,
-    payload: period,
+export const addOfferedPeriods = (periodsToAdd, periodsToExclude = []) => ({
+    type: actionTypes.ADD_OFFERED_PERIODS,
+    periodsToAdd,
+    periodsToExclude,
 });
 
-export const removeSelectedPeriod = id => ({
-    type: actionTypes.REMOVE_SELECTED_PERIOD,
-    payload: id,
+export const removeOfferedPeriods = (periodsToRemove) => ({
+    type: actionTypes.REMOVE_OFFERED_PERIODS,
+    periodsToRemove,
+});
+
+export const toggleOfferedPeriod = (period, index) => ({
+    type: actionTypes.TOGGLE_OFFERED_PERIOD,
+    period,
+    index,
+});
+
+export const addSelectedPeriods = (periodsToAdd) => ({
+    type: actionTypes.ADD_SELECTED_PERIODS,
+    periodsToAdd,
+});
+
+export const removeSelectedPeriods = (periodsToRemove) => ({
+    type: actionTypes.REMOVE_SELECTED_PERIODS,
+    periodsToRemove,
+});
+
+export const toggleSelectedPeriod = (period, index) => ({
+    type: actionTypes.TOGGLE_SELECTED_PERIOD,
+    period,
+    index,
 });
