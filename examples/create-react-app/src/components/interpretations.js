3a7d71cea72e5049659b22a2572e6887dc4f74c1
Arnau Sanchez <pyarnau@gmail.com>
2018-06-06 11:25:59 +0200

    Fix interpretations indentation

--- examples/create-react-app/src/components/interpretations.js
+++ examples/create-react-app/src/components/interpretations.js
@@ -5,11 +5,11 @@ export default class InterpretationsExample extends React.Component {
     render() {
         return (
             <div style={{ padding: 32, width: 400 }}>
-            <Interpretations
-            d2={this.props.d2}
-            id={"zDP78aJU8nX"}
-            type={"map"}
-            />
+                <Interpretations
+                    d2={this.props.d2}
+                    id={"zDP78aJU8nX"}
+                    type={"map"}
+                />
             </div>
         )
     }
