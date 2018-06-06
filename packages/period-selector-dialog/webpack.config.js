5b4c583b6dcd6e155e1d5273d254ffc5c5a71d11
Ilya Nee <neeilya@mail.ru>
2018-06-06 15:13:26 +0200

    Fix handling css for build script

--- packages/period-selector-dialog/webpack.config.js
+++ packages/period-selector-dialog/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
         rules: [
             { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
             { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
         ],
     },
     devtool: 'source-map',
