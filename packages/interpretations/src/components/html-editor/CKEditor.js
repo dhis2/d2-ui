f4c37cb51916184fd4b98442640a2b82dd92ed97
Arnau Sanchez <pyarnau@gmail.com>
2018-06-25 11:02:45 +0200

    [interpretations] Refactor mentions box positioning

--- packages/interpretations/src/components/html-editor/CKEditor.js
+++ packages/interpretations/src/components/html-editor/CKEditor.js
@@ -37,7 +37,7 @@ class WrappedEditor {
         const range = this.editor.getSelection().getRanges()[0];
         const bbox = range ? range.startContainer.$.parentNode.getBoundingClientRect() : {top: 0, left: 0};
         const iframe = this.editor.container.$.getElementsByTagName("iframe")[0];
-        const iframeBbox = iframe.getBoundingClientRect();
+        const iframeBbox = getAbsoluteElementOffset(iframe);
         const pos = { top: bbox.top + iframeBbox.top, left: bbox.left + iframeBbox.left };
         return offset ? { top: pos.top + offset.top, left: pos.left + offset.left } : pos;
     }
@@ -78,6 +78,11 @@ class WrappedEditor {
             return null;
         }
     }
+}
+
+function getAbsoluteElementOffset(el) {
+  const box = el.getBoundingClientRect();
+  return {left: box.left + window.scrollX, top: box.top + window.scrollY};
 }
 
 export default class CKEditor extends Component {
