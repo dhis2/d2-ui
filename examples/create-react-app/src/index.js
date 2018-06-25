44d670f16441a5af4443a9859b377c1146899fce
Arnau Sanchez <pyarnau@gmail.com>
2018-06-25 11:04:29 +0200

    Inject ckeditor into examples app required by interpretations component

--- examples/create-react-app/src/index.js
+++ examples/create-react-app/src/index.js
@@ -6,6 +6,14 @@ import registerServiceWorker from './registerServiceWorker';
 
 import { init } from 'd2/lib/d2'
 
+const ckeditorUrl = '/dhis-web-core-resource/ckeditor/4.6.1/ckeditor.js'
+
+function injectCkeditor(baseUrl) {
+  const ckeditorScript = document.createElement('script');
+  ckeditorScript.setAttribute('src', baseUrl + ckeditorUrl);
+  document.head.appendChild(ckeditorScript);
+}
+
 let baseUrl = process.env.REACT_APP_DHIS2_BASE_URL
 if (!baseUrl) {
 	console.warn('Set the environment variable `REACT_APP_DHIS2_BASE_URL` to your DHIS2 instance to override localhost:8080!')
@@ -15,7 +23,8 @@ if (!baseUrl) {
 init({
     baseUrl: baseUrl + '/api/29'
 }).then(d2 => {
-    console.log(d2)
+    console.log(d2);
+    injectCkeditor(baseUrl);
     ReactDOM.render(<App d2={d2}/>, document.getElementById('root'));
     registerServiceWorker();
 }).catch(e => console.error)
