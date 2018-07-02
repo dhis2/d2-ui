70bf5d65d682a41b184033fcbafec3c86bee56d4
Arnau Sanchez <pyarnau@gmail.com>
2018-07-02 16:40:45 +0200

    [interpretations] Use displayInterpretation on details card (#261)
    * [interpretations] Use displayInterpretation on details card
    
    * [interpretations] Fix models helpers.spec

--- packages/interpretations/src/models/__tests__/helpers.spec.js
+++ packages/interpretations/src/models/__tests__/helpers.spec.js
@@ -38,7 +38,7 @@ describe("getFavoriteWithInterpretations", () => {
 
     describe("api calls", () => {
         it("should call the model D2 get with the required fields", () => {
-            const expectedFields = "id,name,href,user[id,displayName],displayName,description," +
+            const expectedFields = "id,name,href,user[id,displayName],displayName,description,displayDescription," +
                 "created,lastUpdated,access,publicAccess,externalAccess,userAccesses,userGroupAccesses," +
                 "interpretations[id,user[id,displayName,userCredentials[username]],created,likes,likedBy[id,displayName],"
                 + "text,comments[id,text,created,user[id,displayName,userCredentials[username]]]]";
