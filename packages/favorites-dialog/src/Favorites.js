9df050949cf9a2ad473bfc12cdb052c53de60c5c
Edoardo Sabadelli <edoardo@dhis2.org>
2018-10-17 14:34:52 +0200

    Re-fetch list when rename/delete are done via FileMenu

--- packages/favorites-dialog/src/Favorites.js
+++ packages/favorites-dialog/src/Favorites.js
@@ -14,7 +14,7 @@ import { fetchData } from './actions';
 class Favorites extends Component {
     componentWillReceiveProps(nextProps) {
         // fetch data only the first time the dialog is open
-        if (nextProps.open && !nextProps.dataIsLoaded) {
+        if (nextProps.open && (!nextProps.dataIsLoaded || nextProps.refreshData)) {
             this.props.fetchData();
         }
     }
