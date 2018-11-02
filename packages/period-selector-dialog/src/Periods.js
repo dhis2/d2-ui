5e156d0904457cfdb0b04b4174244e92751388d4
Jen Jones Arnesen <jarnesen@baosystems.com>
2018-11-02 15:23:20 +0100

    put css in same dir as js

--- packages/period-selector-dialog/src/Periods.js
+++ packages/period-selector-dialog/src/Periods.js
@@ -8,7 +8,7 @@ import PeriodTypeButton from './PeriodTypeButton';
 import SelectedPeriods from './SelectedPeriods';
 import { OfferedPeriods } from './OfferedPeriods';
 import PeriodTypes from './PeriodTypes';
-import './css/PeriodSelector.css';
+import './PeriodSelector.css';
 
 import {
     setPeriodType,
