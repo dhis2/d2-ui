9742acdf41b4b4e5cc256b4be945f8211a08661b
Austin McGee <>
2018-11-21 15:28:10 +0100

    Fix IE flexbox bug and persistently-open tooltip in Favorites Dialog

--- packages/favorites-dialog/src/EnhancedToolbar.js
+++ packages/favorites-dialog/src/EnhancedToolbar.js
@@ -1,59 +1,82 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import { withStyles } from '@material-ui/core/styles';
-import MenuItem from '@material-ui/core/MenuItem';
-import Select from '@material-ui/core/Select';
-import TextField from '@material-ui/core/TextField';
-import Toolbar from '@material-ui/core/Toolbar';
-import Tooltip from '@material-ui/core/Tooltip';
+import { withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import Toolbar from "@material-ui/core/Toolbar";
+import Tooltip from "@material-ui/core/Tooltip";
 
-import { filterData, searchData } from './actions';
+import { filterData, searchData } from "./actions";
 
 const toolbarStyles = () => ({
     search: {
-        flex: '0 0 auto',
+        flex: "0 0 auto"
     },
     spacer: {
-        flex: '1 1 100%',
+        flex: "1 1 100%"
     },
+    filter: {
+        flex: "0 0 auto"
+    }
 });
 
-let EnhancedToolbar = props => {
-    const {
-        classes,
-        createdByValue,
-        searchValue,
-        searchData,
-        filterData,
-    } = props;
+class EnhancedToolbar extends Component {
+    state = {
+        filterTooltipOpen: false,
+    };
 
-    return (
-        <Toolbar>
-            <TextField
-                type="search"
-                label="Search by name"
-                className={classes.search}
-                value={searchValue}
-                onChange={searchData}
-            />
-            <div className={classes.spacer} />
-            <Tooltip title="Filter list">
-                <Select
-                    disableUnderline={true}
-                    value={createdByValue}
-                    onChange={filterData}
+    showFilterTooltip = () => {
+        this.setState({ filterTooltipOpen: true });
+    };
+    hideFilterTooltip = () => {
+        this.setState({ filterTooltipOpen: false });
+    };
+
+    render() {
+        const {
+            classes,
+            createdByValue,
+            searchValue,
+            searchData,
+            filterData
+        } = this.props;
+
+        return (
+            <Toolbar>
+                <TextField
+                    type="search"
+                    label="Search by name"
+                    className={classes.search}
+                    value={searchValue}
+                    onChange={searchData}
+                />
+                <div className={classes.spacer} />
+                <Tooltip
+                    className={classes.filter}
+                    title="Filter list"
+                    open={this.state.filterTooltipOpen}
                 >
-                    <MenuItem value="all">Show all</MenuItem>
-                    <MenuItem value="byme">Created by me</MenuItem>
-                    <MenuItem value="byothers">Created by others</MenuItem>
-                </Select>
-            </Tooltip>
-        </Toolbar>
-    );
-};
-
-EnhancedToolbar = withStyles(toolbarStyles)(EnhancedToolbar);
+                    <Select
+                        disableUnderline
+                        value={createdByValue}
+                        onChange={filterData}
+                        onMouseEnter={this.showFilterTooltip}
+                        onMouseLeave={this.hideFilterTooltip}
+                        MenuProps={{
+                            onEnter: this.hideFilterTooltip,
+                        }}
+                    >
+                        <MenuItem value="all">Show all</MenuItem>
+                        <MenuItem value="byme">Created by me</MenuItem>
+                        <MenuItem value="byothers">Created by others</MenuItem>
+                    </Select>
+                </Tooltip>
+            </Toolbar>
+        );
+    }
+}
 
 const mapStateToProps = state => ({
     createdByValue: state.filtering.createdByValue,
@@ -65,4 +88,7 @@ const mapDispatchToProps = {
     filterData,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(EnhancedToolbar);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(withStyles(toolbarStyles)(EnhancedToolbar));
