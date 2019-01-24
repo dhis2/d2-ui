4a9e591bca3baa8c052ddc87634bca9009031cfe
Jen Jones Arnesen <jennifer@dhis2.org>
2019-01-24 18:52:55 +0100

    prevent default handler causing actions in firefox

--- packages/rich-text/src/editor/convertCtrlKey.js
+++ packages/rich-text/src/editor/convertCtrlKey.js
@@ -50,8 +50,10 @@ const convertCtrlKey = (event, cb) => {
     state.element = element;
 
     if (key === "b" && (ctrlKey || metaKey)) {
+        event.preventDefault();
         insertMarkers("bold", cb);
     } else if (key === "i" && (ctrlKey || metaKey)) {
+        event.preventDefault();
         insertMarkers("italic", cb);
     }
 }
