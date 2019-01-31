5c57b0c25305348762010cd76813f68fa531a5c9
joakimia <joakimamundsen91@gmail.com>
2019-01-31 10:37:48 +0200

    remove description max length check in DetailsPanel as this potentially removes relevant information

--- packages/interpretations/src/components/DetailsPanel/Description.js
+++ packages/interpretations/src/components/DetailsPanel/Description.js
@@ -3,17 +3,11 @@ import PropTypes from 'prop-types';
 import i18n from '@dhis2/d2-i18n';
 import { Parser as RichTextParser } from '@dhis2/d2-ui-rich-text';
 
-export const Description = ({ description }) => {
-    const descriptionMaxLength = 250;
-    
-    if (!description) {
-        return <i>{i18n.t('No description')}</i>;
-    } else if (description.length < descriptionMaxLength) {
-        return <RichTextParser>{description}</RichTextParser>;
-    } else {
-        return <RichTextParser>{description.substring(0, descriptionMaxLength) + ' ...'}</RichTextParser>
-    }
-};
+export const Description = ({ description }) => (
+    description
+        ? <RichTextParser>{description}</RichTextParser>
+        : <i>{i18n.t('No description')}</i>
+);
 
 export default Description; 
 
