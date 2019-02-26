40c8d14d3f20abe5d5cefca31cd29195ae1d0002
Jen Jones Arnesen <jennifer@dhis2.org>
2019-02-26 17:50:20 +0100

    initial commit

---
+++ packages/item-selector/src/styles/Item.style.js
@@ -0,0 +1,10 @@
+import { colors } from '../colors';
+
+export const styles = {
+    highlightedItem: {
+        backgroundColor: colors.accentSecondaryDark,
+    },
+    highlightedText: {
+        color: colors.white,
+    },
+};
