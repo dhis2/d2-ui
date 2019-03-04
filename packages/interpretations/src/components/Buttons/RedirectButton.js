649b9a045650719beb611076735bf5d9ba519806
joakimia <joakimamundsen91@gmail.com>
2019-03-04 17:05:39 +0100

    fix links to redirect button in itemTypeMap

--- packages/interpretations/src/components/Buttons/RedirectButton.js
+++ packages/interpretations/src/components/Buttons/RedirectButton.js
@@ -11,7 +11,7 @@ export class RedirectButton extends Component {
     render() {
         return this.context.appName === 'dashboard' ? (
             <a
-                href={getLink(this.context.item, this.context.d2)}
+                href={getLink(this.context.item, this.context.d2, this.props.interpretationId)}
                 className={this.props.classes.iconContainer}
                 title={i18n.t(`View in ${itemTypeMap[this.context.item.type].appName} app`)}
             >
@@ -22,6 +22,11 @@ export class RedirectButton extends Component {
             </a>
         ) : null;
     }
+};
+
+RedirectButton.propTypes = {
+    classes: PropTypes.object.isRequired,
+    interpretationId: PropTypes.string.isRequired,
 };
 
 RedirectButton.contextTypes = {
