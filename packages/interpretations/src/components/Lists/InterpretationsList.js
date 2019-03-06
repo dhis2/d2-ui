dad2273650fa7eaababd81343c039cda2c5e8292
joakimia <joakimamundsen91@gmail.com>
2019-03-06 11:04:58 +0100

    show the first 5 interpretations after changing sort order

--- packages/interpretations/src/components/Lists/InterpretationsList.js
+++ packages/interpretations/src/components/Lists/InterpretationsList.js
@@ -24,7 +24,7 @@ export const InterpretationsList = ({
 
     const listItems = isExpanded
         ? filteredItems
-        : filteredItems.slice(-interpretationsToShowOnInit);
+        : filteredItems.slice(0 ,interpretationsToShowOnInit);
 
 
     return listItems.length ? (
