723aaf26509a11e7d31bef02ec2964332746fb1f
Edoardo Sabadelli <edoardo@dhis2.org>
2019-03-26 15:51:16 +0100

    Add internally used shared components

---
+++ packages/analytics/src/components/styles/DimensionsPanel.style.js
@@ -0,0 +1,15 @@
+import { colors } from '../../../modules/colors';
+
+export const styles = {
+    divContainer: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: colors.snow,
+        padding: '8px 0 2px 8px',
+    },
+    textField: {
+        paddingBottom: '12px',
+        width: 'calc(100% - 8px)',
+    },
+};
