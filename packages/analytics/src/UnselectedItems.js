0d4f7885a55232f761b57a2b5f602b13f967bb7e
Jen Jones Arnesen <jennifer@dhis2.org>
2019-04-02 19:07:04 +0200

    fix throttle fn call

--- packages/analytics/src/UnselectedItems.js
+++ packages/analytics/src/UnselectedItems.js
@@ -78,7 +78,7 @@ export class UnselectedItems extends Component {
         </li>
     );
 
-    requestMoreItems = throttle(1000, () => {
+    requestMoreItems = throttle(() => {
         const node = this.scrolElRef.current;
 
         if (node) {
@@ -88,7 +88,7 @@ export class UnselectedItems extends Component {
                 this.props.requestMoreItems();
             }
         }
-    });
+    }, 1000);
 
     render = () => {
         const listItems = this.props.items.map((item, index) =>
