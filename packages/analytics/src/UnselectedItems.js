ee4686209854fb629a7e617b99e3b8507f111052
Jen Jones Arnesen <jennifer@dhis2.org>
2019-04-02 14:58:36 +0200

    add package-level babelrc and use regular lodash sortBy

--- packages/analytics/src/UnselectedItems.js
+++ packages/analytics/src/UnselectedItems.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import i18n from '@dhis2/d2-i18n';
 import Button from '@dhis2/ui/core/Button';
-import throttle from 'lodash/fp/throttle';
+import throttle from 'lodash/throttle';
 
 import Item from './widgets/UnselectedItem';
 import { ArrowButton as AssignButton } from './widgets/ArrowButton';
