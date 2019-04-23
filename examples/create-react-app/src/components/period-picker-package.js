0fabd10bc3a490dd0959839204461ada19332af5
Hendrik de Graaf <hendrik@dhis2.org>
2019-04-23 16:15:48 +0200

    feat(period-picker): improved example

--- examples/create-react-app/src/components/period-picker-package.js
+++ examples/create-react-app/src/components/period-picker-package.js
@@ -1,27 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Card, CardText } from 'material-ui/Card';
 import PeriodPicker from '@dhis2/d2-ui-period-picker';
+import parsePeriod from 'd2/period/parser';
 
 const DUMMY_PERIOD = '2017ThuW4';
 
 export default class PeriodPickerPackageExample extends React.Component {
     state = {
-        periodTypes: [],
-        selectedPeriod: null,
+        selectedPeriod: '',
         selectedPeriods: [],
+        periodIdStr: DUMMY_PERIOD,
+    };
+
+    updatePeriodIdStr = event => {
+        this.setState({ periodIdStr: event.target.value });
     };
 
     onPeriodChange = period => {
         this.setState({
-            selectedPeriods: [...this.state.selectedPeriods, period],
+            selectedPeriod: period,
+            selectedPeriods: [
+                ...this.state.selectedPeriods,
+                parsePeriod(period),
+            ],
         });
     };
 
-    setDummySelectedPeriod = () => {
-        this.setState({ selectedPeriod: DUMMY_PERIOD });
+    setPeriodFromParent = () => {
+        this.setState({
+            selectedPeriod: this.state.periodIdStr,
+        });
     };
-
-    componentDidMount() {}
 
     render() {
         const styles = {
@@ -39,6 +49,22 @@ export default class PeriodPickerPackageExample extends React.Component {
                 margin: '16px -16px',
                 borderBottom: '1px solid #eeeeee',
             },
+            table: {
+                width: '100%',
+                marginBottom: 8,
+            },
+            th: {
+                textAlign: 'left',
+            },
+            input: {
+                marginRight: 8,
+            },
+            demoElementContainer: {
+                backgroundColor: '#eaeaea',
+                width: 'calc(100% - 8px)',
+                padding: 8,
+                marginTop: 16,
+            },
         };
 
         styles.cardWide = Object.assign({}, styles.card, {
@@ -54,23 +80,52 @@ export default class PeriodPickerPackageExample extends React.Component {
                         </h3>
                         <div className="scroll">
                             <PeriodPicker
-                                periodTypes={this.state.periodTypes}
-                                selectedPeriod={this.state.selectedPeriod}
+                                d2={this.context.d2}
+                                label="Report period"
+                                value={this.state.selectedPeriod}
                                 onChange={this.onPeriodChange}
                             />
                         </div>
-                        <button onClick={this.onPeriodChange}>
-                            Set period to {DUMMY_PERIOD} via props
-                        </button>
-                        <ol>
-                            {this.state.selectedPeriods.map((v, i) => (
-                                <li key={i}>{v}</li>
-                            ))}
-                        </ol>
+                        <div style={styles.demoElementContainer}>
+                            {this.state.selectedPeriods.length > 0 && (
+                                <table style={styles.table}>
+                                    <thead>
+                                        <tr>
+                                            <th style={styles.th}>ID</th>
+                                            <th style={styles.th}>
+                                                Display value
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.selectedPeriods.map(
+                                            (v, i) => (
+                                                <tr key={i}>
+                                                    <td>{v.id}</td>
+                                                    <td>{v.name}</td>
+                                                </tr>
+                                            )
+                                        )}
+                                    </tbody>
+                                </table>
+                            )}
+                            <input
+                                type="text"
+                                onChange={this.updatePeriodIdStr}
+                                value={this.state.periodIdStr}
+                                style={styles.input}
+                            />
+                            <button onClick={this.setPeriodFromParent}>
+                                Set period via props
+                            </button>
+                        </div>
                     </CardText>
-                    <ol>{this.state.selectedPeriods}</ol>
                 </Card>
             </div>
         );
     }
 }
+
+PeriodPickerPackageExample.contextTypes = {
+    d2: PropTypes.object,
+};
