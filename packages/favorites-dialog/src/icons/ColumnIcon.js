45516bdbad851d9207114f75669e14430c2e3be7
Edoardo Sabadelli <edoardo@dhis2.org>
2020-01-17 12:38:46 +0100

    fix: add right margin on icons
    Need some spacing with the text in the menu.

--- packages/favorites-dialog/src/icons/ColumnIcon.js
+++ packages/favorites-dialog/src/icons/ColumnIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SvgIcon from '@material-ui/core/SvgIcon';
 
-export default <SvgIcon viewBox="0 0 16 16">
+export default <SvgIcon viewBox="0 0 16 16" style={{ marginRight: 8 }}>
     <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
         <g fill="#7B8998">
             <rect x="0" y="15" width="16" height="1"></rect>
