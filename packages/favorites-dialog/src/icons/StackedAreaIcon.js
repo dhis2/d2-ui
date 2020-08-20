652466fc47273b824753361dd3edd8594cfe23ec
Edoardo Sabadelli <edoardo@dhis2.org>
2020-08-20 15:10:44 +0200

    feat: support Stacked area vis type in open dialog (DHIS2-9217) (#621)

---
+++ packages/favorites-dialog/src/icons/StackedAreaIcon.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import SvgIcon from '@material-ui/core/SvgIcon';
+
+export default <SvgIcon viewBox="0 0 16 16" style={{ fontSize: 16 }}>
+    <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <path d="m0 0h1v15h15v1h-16z" fill="#4a5768"/>
+        <path d="m15 9v5h-13v-3l4-2 4 3zm-5-6 5 2v2l-5 3-4-3-4 2v-6l4 2z" fill="#7b8998"/>
+    </g>
+</SvgIcon>;
