722a47ee79a5c0aa66caff6de958279239cf8c09
Jen Jones Arnesen <jennifer@dhis2.org>
2020-11-25 13:56:23 +0100

    fix: custom i18n extract script to insert keys as the default value in pot file

---
+++ packages/interpretations/scripts/extract.js
@@ -0,0 +1,105 @@
+const fs = require("fs-extra");
+const path = require("path");
+const {
+    checkDirectoryExists,
+    walkDirectory,
+    arrayEqual
+} = require("./helpers");
+const { reporter, chalk } = require("@dhis2/cli-helpers-engine");
+const scanner = require("i18next-scanner");
+const { i18nextToPot, gettextToI18next } = require("i18next-conv");
+
+/**
+ * This script is a near exact copy of the cli-app-scripts extract script. The
+ * only difference is that this script sets the default value, which results
+ * in the en.pot file containing all the translations instead of empty msgstr.
+ *
+ * If this approach is successful for handling plurals, then the fix will be
+ * applied to cli-app-scripts
+ */
+
+const extract = async ({ input, output }) => {
+    const relativeInput = "./" + path.relative(process.cwd(), input);
+    if (!checkDirectoryExists(input)) {
+        reporter.error(
+            `I18n source directory ${chalk.bold(relativeInput)} does not exist.`
+        );
+        return false;
+    }
+
+    reporter.debug(`[i18n-extract] Reading ${chalk.bold(relativeInput)}...`);
+
+    const files = walkDirectory(input);
+    if (files.length === 0) {
+        reporter.error(
+            `Directory ${chalk.bold(relativeInput)} has no files to translate.`
+        );
+        process.exit(1);
+    }
+
+    var parser = new scanner.Parser({
+        keepRemoved: false,
+        keySeparator: false,
+        sort: true,
+        defaultValue: (lng, ns, key, options) => {
+            return options.defaultValue ? options.defaultValue : key;
+        }
+    });
+
+    reporter.debug(`[i18n-extract] Parsing ${files.length} files...`);
+
+    files.forEach(filePath => {
+        var contents = fs.readFileSync(filePath, "utf8");
+        parser.parseFuncFromString(contents).get();
+    });
+
+    var parsed = parser.get();
+    var en = {};
+    Object.keys(parsed.en.translation).forEach(str => {
+        en[str] = parsed.en.translation[str];
+    });
+
+    if (Object.keys(en).length === 0) {
+        reporter.print(
+            chalk.dim(
+                `No translatable strings found in directory ${chalk.bold(
+                    relativeInput
+                )}`
+            )
+        );
+        return true;
+    }
+
+    var targetPath = path.join(output, "en.pot");
+
+    if (fs.existsSync(targetPath)) {
+        // validate, diff translation keys b/w en.pot vs now
+        const content = fs.readFileSync(targetPath, "utf8");
+        const json = await gettextToI18next("en", content);
+
+        var msgIds = Object.keys(en);
+        var newMsgIds = Object.keys(JSON.parse(json));
+
+        if (arrayEqual(newMsgIds, msgIds)) {
+            reporter.print(chalk.dim("No i18n updates found!"));
+            return true;
+        }
+    }
+
+    reporter.print(
+        chalk.dim(
+            `Writing ${Object.keys(en).length} language strings to ${chalk.bold(
+                "./" + targetPath
+            )}...`
+        )
+    );
+
+    const result = await i18nextToPot("en", JSON.stringify(en));
+
+    fs.ensureFileSync(targetPath);
+    fs.writeFileSync(targetPath, result + "\n");
+    reporter.debug("[i18n-extract] complete");
+    return true;
+};
+
+extract({ input: "./src/", output: "./i18n/" });
---
+++ packages/interpretations/scripts/helpers.js
@@ -0,0 +1,51 @@
+const { reporter, chalk } = require("@dhis2/cli-helpers-engine");
+const fs = require("fs-extra");
+const path = require("path");
+
+const supportedExtensions = [".js", ".jsx", ".ts", ".tsx"];
+
+module.exports.checkDirectoryExists = dir => {
+    const dirPath = path.normalize(dir);
+    try {
+        const stat = fs.lstatSync(dirPath);
+
+        if (!stat.isDirectory()) {
+            reporter.debugErr(
+                `Directory ${chalk.bold(dirPath)} is not a directory.`
+            );
+            return false;
+        }
+    } catch (e) {
+        reporter.debugErr(`Directory ${chalk.bold(dirPath)} does not exist.`);
+        return false;
+    }
+    return true;
+};
+
+// src from component/array-equal
+module.exports.arrayEqual = (arr1, arr2) =>
+    arr1.length === arr2.length &&
+    arr1.every((item, idx) => item === arr2[idx]);
+
+function walkDirectory(dirPath, files = []) {
+    const list = fs.readdirSync(dirPath);
+
+    list.forEach(fileName => {
+        const filePath = path.join(dirPath, fileName);
+        const stat = fs.lstatSync(filePath);
+
+        if (stat.isDirectory()) {
+            walkDirectory(filePath, files);
+        } else if (stat.isFile() && !stat.isSymbolicLink()) {
+            const ext = path.extname(fileName);
+
+            if (supportedExtensions.includes(ext)) {
+                files.push(filePath);
+            }
+        }
+    });
+
+    return files;
+}
+
+module.exports.walkDirectory = walkDirectory;
