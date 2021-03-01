c7213370b8b62a7b35143e6b2dd290cd39715f27
Edoardo Sabadelli <edoardo@dhis2.org>
2021-03-01 10:18:31 +0100

    chore: run linter

--- packages/favorites-dialog/src/EnhancedToolbar.js
+++ packages/favorites-dialog/src/EnhancedToolbar.js
@@ -1,45 +1,55 @@
-import React, { Fragment } from "react";
-import { connect } from "react-redux";
+import React, { Fragment } from 'react';
+import { connect } from 'react-redux';
 
-import { withStyles } from "@material-ui/core/styles";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
-import TextField from "@material-ui/core/TextField";
-import Toolbar from "@material-ui/core/Toolbar";
-import Divider from "@material-ui/core/Divider";
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import Toolbar from '@material-ui/core/Toolbar';
+import Divider from '@material-ui/core/Divider';
 
 import i18n from '@dhis2/d2-i18n';
-import { filterData, searchData } from "./actions";
-import { CHART, PIVOT_TABLE, COLUMN, getVisTypeLabel, visTypeIcons } from "./visTypes";
+import { filterData, searchData } from './actions';
+import {
+    CHART,
+    PIVOT_TABLE,
+    COLUMN,
+    getVisTypeLabel,
+    visTypeIcons,
+} from './visTypes';
 
 const toolbarStyles = () => ({
     search: {
-        flex: "0 0 auto"
+        flex: '0 0 auto',
     },
     spacer: {
-        flex: "1 1 100%"
+        flex: '1 1 100%',
     },
     filter: {
-        marginLeft: 8
+        marginLeft: 8,
     },
     menuItem: {
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
     },
     menuIcon: {
         marginRight: 8,
-        height: 16
+        height: 16,
     },
 });
 
-const VisTypeFilterMenuItem = withStyles(toolbarStyles)(({ classes, type, icon = undefined, label = undefined }) => (
+const VisTypeFilterMenuItem = withStyles(toolbarStyles)(
+    ({ classes, type, icon = undefined, label = undefined }) => (
         <span className={classes.menuItem}>
-            <span className={classes.menuIcon}>{icon || visTypeIcons[type]}</span>
+            <span className={classes.menuIcon}>
+                {icon || visTypeIcons[type]}
+            </span>
             {label || getVisTypeLabel(type)}
         </span>
-));
+    )
+);
 
-const EnhancedToolbar = props => {
+const EnhancedToolbar = (props) => {
     const {
         classes,
         createdByValue,
@@ -47,64 +57,72 @@ const EnhancedToolbar = props => {
         visTypeValue,
         searchData,
         filterData,
-        showTypeFilter
+        showTypeFilter,
     } = props;
 
     return (
         <Toolbar>
             <TextField
                 type="search"
-                label={i18n.t("Search by name")}
+                label={i18n.t('Search by name')}
                 className={classes.search}
                 value={searchValue}
                 onChange={searchData}
             />
 
             <div className={classes.spacer} />
-                <Fragment>
-                    {showTypeFilter ?
-                        <Select
-                            className={classes.filter}
-                            disableUnderline
-                            value={visTypeValue}
-                            onChange={event => filterData('visType', event.target.value)}
-                        >
-                            <MenuItem value="all">{i18n.t('All types')}</MenuItem>
-                            <Divider />
-                            <MenuItem value={CHART}>
-                                <VisTypeFilterMenuItem
-                                    type={CHART}
-                                    icon={visTypeIcons[COLUMN]}
-                                    label={i18n.t('All chart types')}
-                                />
-                            </MenuItem>
-                            <MenuItem value={PIVOT_TABLE}>
-                                <VisTypeFilterMenuItem type={PIVOT_TABLE} />
-                            </MenuItem>
-                            <Divider />
-                            {Object.keys(visTypeIcons).filter(type => type !== PIVOT_TABLE).map(type =>
-                                <MenuItem key={type} value={type}>
-                                    <VisTypeFilterMenuItem type={type} />
-                                </MenuItem>
-                            )}
-                        </Select> : null
-                    }
+            <Fragment>
+                {showTypeFilter ? (
                     <Select
                         className={classes.filter}
                         disableUnderline
-                        value={createdByValue}
-                        onChange={event => filterData('owner', event.target.value)}
+                        value={visTypeValue}
+                        onChange={(event) =>
+                            filterData('visType', event.target.value)
+                        }
                     >
-                        <MenuItem value="all">{i18n.t('All owners')}</MenuItem>
-                        <MenuItem value="byme">{i18n.t('Created by you')}</MenuItem>
-                        <MenuItem value="byothers">{i18n.t('Created by others')}</MenuItem>
+                        <MenuItem value="all">{i18n.t('All types')}</MenuItem>
+                        <Divider />
+                        <MenuItem value={CHART}>
+                            <VisTypeFilterMenuItem
+                                type={CHART}
+                                icon={visTypeIcons[COLUMN]}
+                                label={i18n.t('All chart types')}
+                            />
+                        </MenuItem>
+                        <MenuItem value={PIVOT_TABLE}>
+                            <VisTypeFilterMenuItem type={PIVOT_TABLE} />
+                        </MenuItem>
+                        <Divider />
+                        {Object.keys(visTypeIcons)
+                            .filter((type) => type !== PIVOT_TABLE)
+                            .map((type) => (
+                                <MenuItem key={type} value={type}>
+                                    <VisTypeFilterMenuItem type={type} />
+                                </MenuItem>
+                            ))}
                     </Select>
-                </Fragment>
+                ) : null}
+                <Select
+                    className={classes.filter}
+                    disableUnderline
+                    value={createdByValue}
+                    onChange={(event) =>
+                        filterData('owner', event.target.value)
+                    }
+                >
+                    <MenuItem value="all">{i18n.t('All owners')}</MenuItem>
+                    <MenuItem value="byme">{i18n.t('Created by you')}</MenuItem>
+                    <MenuItem value="byothers">
+                        {i18n.t('Created by others')}
+                    </MenuItem>
+                </Select>
+            </Fragment>
         </Toolbar>
     );
-}
+};
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     createdByValue: state.filtering.createdByValue,
     searchValue: state.filtering.searchValue,
     visTypeValue: state.filtering.visTypeValue,
